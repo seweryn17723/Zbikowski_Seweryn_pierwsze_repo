afc41c18c6174fcbc5ea4c69c31bb1ef92799dc8
seweryn17723 <161207556+seweryn17723@users.noreply.github.com>
2024-02-25 12:43:24 +0100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.version)
+
+#git config --lsit
+
+#q - w terminalu zamyka polecenie
+#strzalkami mozna sie przemieszczac
+
+
+#git config --global user.name "seweryn17723"
+#git config --global user.email "161207556+seweryn17723@users.noreply.github.com"
+
