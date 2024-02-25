0572a733569f81cb08330ef6c0735985cf0a5a4a
seweryn17723 <161207556+seweryn17723@users.noreply.github.com>
2024-02-25 13:20:38 +0100

    dzialania

--- Main.py
+++ Main.py
@@ -12,8 +12,30 @@ print(sys.version)
 #git config --global user.email "161207556+seweryn17723@users.noreply.github.com"
 
 
+
+
+#aktualizacja repozytorium/projektu na innym urządzeniu lewy górny róg Update Project, niebieska strzalka
+
+#Dzialania na zmiennych
+
 a = 'sdasdasd'
 print(a)
 
 b = 'sadasda'
 print(b)
+
+x = int
+x = input("Podaj x")
+y = 15
+z = 5
+c = a + b
+
+print(c, "\n")
+print(x + z, "\n")
+print(x/z, "\n")
+print(x * y, "\n")
+print(y%x, "\n")
+print(x-z, "\n")
+
+if (x == 10): print("x is 10", "\n")
+else: print("x is not 10", "\n")
